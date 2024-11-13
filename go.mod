a724c0c362da5a2587e0f7e9d480fc9b3fb7e2ca
Rakesh Gain <rakeshgain47681@gmail.com>
2024-11-14 01:24:57 +0530

    dependency mgmt puppy

---
+++ go.mod
@@ -0,0 +1,5 @@
+module dependency_mgmt
+
+go 1.22.2
+
+require github.com/mrGain/Puppy v0.0.0-20241113185531-e9548113a8c4 // indirect
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/mrGain/Puppy"
+)
+
+func main(){
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+}
